a3c16e52f1f95048b05b4982c9e955caf21f59e2
tandorost <amrimohammadhamzehi@gmail.com>
2025-06-10 21:16:08 +0330

    feat: add validation for input

--- register_user.py
+++ register_user.py
@@ -5,8 +5,11 @@ from telegram.ext import (
     ConversationHandler, CallbackContext
 )
 from config import API_BASE_URL
+from to_english_digits import to_english_digits
+from validation_input import validate_english_username, validate_numeric_input
+
 API_URL = f"{API_BASE_URL}/auth/register/"
-USERNAME_REGISTER, PHONE_REGISTER = range(2)
+USERNAME_REGISTER_USER, PHONE_REGISTER_USER = range(2)
 
 
 async def start(update: Update, context: CallbackContext):
@@ -18,22 +21,24 @@ async def start(update: Update, context: CallbackContext):
     
     """
     await update.message.reply_text(text)
-    return USERNAME_REGISTER
+    return USERNAME_REGISTER_USER
 
 
+@validate_english_username(USERNAME_REGISTER_USER)
 async def get_username(update: Update, context: CallbackContext):
     context.user_data["username"] = update.message.text
 
     await update.message.reply_text("لطفاً شماره تلفن خود را وارد کنید:")
-    return PHONE_REGISTER
+    return PHONE_REGISTER_USER
 
 
+@validate_numeric_input(PHONE_REGISTER_USER)
 async def get_phone(update: Update, context: CallbackContext):
-    phone_number = update.message.text
+    phone_number = to_english_digits(update.message.text.strip())
 
     if not phone_number.isdigit() or len(phone_number) != 11:
         await update.message.reply_text("❌ شماره تلفن نامعتبر است! لطفاً شماره ۱۱ رقمی معتبر وارد کنید:")
-        return PHONE_REGISTER
+        return PHONE_REGISTER_USER
 
     context.user_data["phone_number"] = phone_number
 
@@ -45,7 +50,7 @@ async def get_phone(update: Update, context: CallbackContext):
 
     response = requests.post(API_URL, json=data)
 
-    if response.status_code == 200:
+    if response.status_code == 201:
         await update.message.reply_text("ثبت نام شما با موفقیت انجام شد")
     else:
         await update.message.reply_text(response.json())
