8ff44f581dfaa2f62eff62ee4fcc14327c554b4d
AmirMohammad Hamzeh <110425654+AmirmohammadHamzeh@users.noreply.github.com>
2025-09-04 14:23:35 +0330

    Delete get_user_token.py

--- get_user_token.py
+++
@@ -1,7 +0,0 @@
-import redis.asyncio as aioredis
-
-redis_client = aioredis.Redis(host="localhost", port=6379, decode_responses=True)
-
-
-async def get_user_token(user_id: int) -> str:
-    return await redis_client.get(f"user:{user_id}:token")
