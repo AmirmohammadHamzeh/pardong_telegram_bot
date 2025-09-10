3af527515482eb2792db79cb30fbd10f3be8818c
tandorost <amrimohammadhamzehi@gmail.com>
2025-09-11 00:48:34 +0330

    feat : use python-dotenv for token bot

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-TOKEN = "8248595159:AAGDeUpIQJceeiUODv6G9UKpk1GkBdoTIiQ"
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 API_BASE_URL = "http://127.0.0.1:8000"
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
