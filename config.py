594faeb007ca942ce0befa9888fe88085cd22486
tandorost <amrimohammadhamzehi@gmail.com>
2025-11-09 13:13:47 +0330

    chore : update config file

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-API_BASE_URL = "http://127.0.0.1:8000"
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+TOKEN = os.environ.get("BOT_TOKEN")
+API_BASE_URL = os.environ.get("API_URL")
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 DB_NUM_CACHE_GROUP_ID = 1
 DB_NUM_CACHE_EXPENSE_ID = 2
